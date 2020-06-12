d916467cf07c165192f2d32c6a307b4c2c86200a
Adrian Rios <adrian275@gmail.com>
2020-06-11 21:56:44 -0700

    Passport

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const connect = mongoose.connect(url, {
 
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 connect.then(() => console.log('Connected correctly to server'), err => console.log(err)
 );
@@ -44,24 +46,21 @@ app.use(session({
   store: new FileStore()//create new file store as object, save session info to server hard disc
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
-      const err = new Error('You are not authenticated!');
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
       err.status = 401;
       return next(err);
   } else {
-    if (req.session.user === 'authenticated') {
       return next();
-    } else {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    }
   }
 }
 
