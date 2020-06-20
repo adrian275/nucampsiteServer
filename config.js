44645cb51fbaa0d082d7183d913542c8fa5087e3
Adrian Rios <adrian275@gmail.com>
2020-06-19 19:55:05 -0700

    OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '576942413208033',
+        clientSecret: '9d414c84838d9cda1f14559639df4048'
+    }
 }
